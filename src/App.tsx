95f8720d957dfd546a6bdf5d795a4d5ca82e065d
Vinh Hoang <vinh.hoang@employmenthero.com>
2022-04-21 16:48:10 +0700

    Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,36 @@
 import React from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
+import { Layout, Menu } from 'antd';
+import { HomeOutlined, MessageOutlined } from '@ant-design/icons';
+
 import AccountSelectionPage from './pages/AccountSelectionPage';
 import ConversationsPage from './pages/ConversationsPage';
 
 import './App.css';
 
+const { Header, Content } = Layout;
+
 function App() {
   return (
-    <div className="App">
-      <h2>Welcome to the messenger app!</h2>
-      <Link to="/">Account Selection</Link>
-      <br />
-      <Link to="/conversations">Conversations</Link>
-
-      <Routes>
-        <Route path="/" element={<AccountSelectionPage />} />
-        <Route path="/conversations" element={<ConversationsPage />} />
-      </Routes>
-    </div>
+    <Layout className="layout">
+      <Header>
+        <div className="logo" />
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+          <Menu.Item key="home" icon={<HomeOutlined />}>
+            <Link to="/">Account Selection</Link>
+          </Menu.Item>
+          <Menu.Item key="conversations" icon={<MessageOutlined />}>
+            <Link to="/conversations">Conversations</Link>
+          </Menu.Item>
+        </Menu>
+      </Header>
+      <Content style={{ padding: '0 50px' }}>
+        <Routes>
+          <Route path="/" element={<AccountSelectionPage />} />
+          <Route path="/conversations" element={<ConversationsPage />} />
+        </Routes>
+      </Content>
+    </Layout>
   );
 }
 
