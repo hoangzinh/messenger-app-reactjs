40907e6741a31c30fd7afa043c6f831e8e3b18eb
Vinh Hoang <vinh.hoang@employmenthero.com>
2022-04-22 15:44:38 +0700

    fix warning less

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import './index.css';
-import 'antd/dist/antd.css';
+import 'antd/dist/antd.min.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
