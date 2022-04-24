c5f80a7f92d708795c479902cff57b1823c1357b
Vinh Hoang <vinh.hoang@employmenthero.com>
2022-04-24 07:54:17 +0700

    update cros proxy

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,2 @@
 export const API_DOMAIN: string =
-  'https://justcors.com/tl_b99baf5/https://messenger-api-nextjs.herokuapp.com';
+  'https://justcors.com/tl_b633760/https://messenger-api-nextjs.herokuapp.com';
