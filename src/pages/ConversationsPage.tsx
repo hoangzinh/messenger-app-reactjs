12b0cf40c9a25bc9d47d3f09b81c91f5f1c0679a
Vinh Hoang <vinh.hoang@employmenthero.com>
2022-04-29 23:00:52 +0700

    handle accountId is null

--- src/pages/ConversationsPage.tsx
+++ src/pages/ConversationsPage.tsx
@@ -1,11 +1,18 @@
 import { Divider } from 'antd';
 import { useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 import Conversation from '../components/Conversation';
 import ConversationList from '../components/ConversationList';
+import UnexpectedError from '../components/UnexpectedError';
 
 const ConversationsPage = () => {
   const [activeConversation, setActiveConversation] = useState<string>();
+  const { id: accountId } = useParams();
+
+  if (!accountId) {
+    return <UnexpectedError />;
+  }
 
   return (
     <div style={{ display: 'flex', height: '100%', alignItems: 'stretch' }}>
@@ -13,7 +20,11 @@ const ConversationsPage = () => {
       <Divider type="vertical" style={{ height: '100%', margin: 0 }} />
       <div style={{ display: 'flex', flexDirection: 'column', flexGrow: 1 }}>
         {activeConversation ? (
-          <Conversation key={activeConversation} id={activeConversation} />
+          <Conversation
+            key={activeConversation}
+            id={activeConversation}
+            accountId={accountId}
+          />
         ) : null}
       </div>
     </div>
