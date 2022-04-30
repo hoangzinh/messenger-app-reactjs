6f63b0c4b91fbd03d4eb23ecbe016ce08247269e
Vinh Hoang <vinh.hoang@employmenthero.com>
2022-04-30 15:16:14 +0700

    revert change on constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,3 @@
-// export const API_DOMAIN: string =
-//   'https://cors-anywhere-beast.herokuapp.com/https://messenger-api-nextjs.herokuapp.com';
-export const API_DOMAIN: string = '';
-export const POLLING_TIME_MS: number = 2000;
+export const API_DOMAIN: string =
+  'https://cors-anywhere-beast.herokuapp.com/https://messenger-api-nextjs.herokuapp.com';
+export const POLLING_TIME_MS: number = 500;
